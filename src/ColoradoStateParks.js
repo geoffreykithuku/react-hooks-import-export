e013926f6110c4955077d58609a9590ea8e27706
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-09-28 13:17:37 +0300

    resubmission

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -5,7 +5,6 @@ import MesaVerde from "./parks/MesaVerde";
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return (<h1>Colorado State Parks!</h1>
-    
-  );
+  return <h1>Colorado State Parks!</h1>;
 }
+export default ColoradoStateParks;
